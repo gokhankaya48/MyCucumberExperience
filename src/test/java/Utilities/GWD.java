ef2e33727209e6e1d57bf0f836d05451975c0dba
gokhankaya <gokhankaya48@icloud.com>
2024-03-28 21:42:35 +0300

    update

--- src/test/java/Utilities/GWD.java
+++ src/test/java/Utilities/GWD.java
@@ -6,22 +6,38 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 import java.util.Locale;
 
+
 public class GWD {
-    private static WebDriver driver;
 
-    public static WebDriver getDriver() {
+    private static ThreadLocal<WebDriver> threadDriver=new ThreadLocal<>(); //her bir thread e özel static
+    private static ThreadLocal<String> threadBrowserName=new ThreadLocal<>();
 
-            Locale.setDefault(new Locale("EN"));
-            System.setProperty("user.language", "EN");
+    //  threadDriver.get() -> bulunduğu hat taki driverı ver
+    //  threadDriver.set(driver) -> bulunduğu hata driver set et
 
-        if (driver == null) {
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+    // Her bir sürecin kendine özel STATİC driverı olmalı : Local Static diyeceğiz
+    // süreç = Thread
+    // Thread.Sleep => ilgili süreci durduryor belli süre
+    // her bir Thread in kendine özel STATİC i olmalı, yani LOCAL Static
+
+
+    public static WebDriver getDriver(){
+
+        Locale.setDefault(new Locale("EN"));
+        System.setProperty("user.language", "EN");
+
+        if (threadDriver.get() == null) { // hatta driver var mı, yok ise
+            threadDriver.set(new ChromeDriver());  // bir tane oluştur hatta set et
+            threadDriver.get().manage().window().maximize();
+            threadDriver.get().manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
         }
-        return driver;
+
+        return threadDriver.get();
     }
+
+
     public static void quitDriver(){
+
         //test sonucu ekranı bir miktar beklesin diye
         try {
             Thread.sleep(5000);
@@ -29,11 +45,14 @@ public class GWD {
             throw new RuntimeException(e);
         }
 
-        if (driver!=null){// driver var ise
-            driver.quit();
-            driver=null;
+        //driver kapat
+        if (threadDriver.get() != null){ //driver var ise
+            threadDriver.get().quit();
+            WebDriver driver=null;
+            threadDriver.set(driver);
         }
 
     }
+
 
 }
