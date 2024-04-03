36e38908d7824622f67e0a9ff21038c53d600164
gokhankaya <gokhankaya48@icloud.com>
2024-04-03 21:57:48 +0300

    update

--- src/test/java/Utilities/ExelUtility.java
+++ src/test/java/Utilities/ExelUtility.java
@@ -1,9 +1,6 @@
 package Utilities;
 
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
-
+import org.apache.poi.ss.usermodel.*;
 import java.io.FileInputStream;
 import java.util.ArrayList;
 
