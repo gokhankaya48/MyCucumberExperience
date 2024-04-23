c22acb45e002b0c06ae2ea8a506b96446994e7a5
gokhankaya <gokhankaya48@icloud.com>
2024-04-23 11:59:26 +0300

    update

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -5,7 +5,6 @@ import Utilities.GWD;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
